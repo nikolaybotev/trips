2cceda7964d7761db2519886c7fc7cb479f6afa0
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-09-13 20:04:35 -0700

    Tune the auto-scaling even more: fix min_num_workers, enable dynamic thread scaling.

--- dataflow/run_dataflow_job.sh
+++ dataflow/run_dataflow_job.sh
@@ -5,44 +5,42 @@
 PROJECT_ID="feelinsosweet"
 REGION="us-east1"
 WORKER_SERVICE_ACCOUNT="dataflow-worker@feelinsosweet.iam.gserviceaccount.com"
-INPUT_FILE="gs://feelinsosweet-starburst/trips-iceberg/data/trip_start_time_hour=2025-08-12-1**"
+INPUT_FILES="gs://feelinsosweet-starburst/trips-iceberg/data/trip_start_time_hour=2025-08-12-1**"
 OUTPUT_PREFIX="gs://feelinsosweet-starburst/staypoints-hive"
 TEMP_LOCATION="gs://feelinsosweet-dataflow/temp"
 STAGING_LOCATION="gs://feelinsosweet-dataflow/staging"
 CONTAINER_IMAGE_URL="us-east1-docker.pkg.dev/feelinsosweet/trips-to-staypoints-dataflow/trips-to-staypoints-dataflow"
-
-# Dataflow job options
-RUNNER="DataflowRunner"  # Use DirectRunner for local testing
-
-# Needed by DataflowRunner
-export GOOGLE_CLOUD_PROJECT=$PROJECT_ID
+JOB_NAME="trips-to-staypoints-$(date +%Y%m%d-%H%M%S)"
 
 echo "Starting Dataflow job..."
 echo "Project: $PROJECT_ID"
 echo "Region: $REGION"
-echo "Input: $INPUT_FILE"
+echo "Job name: $JOB_NAME"
+echo "Input: $INPUT_FILES"
 echo "Output: $OUTPUT_PREFIX"
 echo
 
 # Start the Dataflow job with the following pipeline options:
 # See https://cloud.google.com/dataflow/docs/reference/pipeline-options#pythonyaml_3
+export GOOGLE_CLOUD_PROJECT="$PROJECT_ID" # Needed by DataflowRunner
 python trips_to_staypoints_dataflow.py \
-    --input="$INPUT_FILE" \
+    --input="$INPUT_FILES" \
     --output="$OUTPUT_PREFIX" \
     --output_format=hive \
     --project="$PROJECT_ID" \
     --region="$REGION" \
-    --runner="$RUNNER" \
+    --runner="DataflowRunner" \
     --temp_location="$TEMP_LOCATION" \
     --staging_location="$STAGING_LOCATION" \
     --service_account_email="$WORKER_SERVICE_ACCOUNT" \
-    --job_name="trips-to-staypoints-$(date +%Y%m%d-%H%M%S)" \
-    --min_num_workers=18 \
+    --job_name="$JOB_NAME" \
+    --dataflow_service_options=min_num_workers=18 \
     --max_num_workers=18 \
     --disk_size_gb=25 \
     --worker_disk_type=compute.googleapis.com/projects/$PROJECT_ID/zones/$REGION/diskTypes/pd-ssd \
     --experiments=use_runner_v2 \
     --dataflow_service_options=enable_prime \
+    --dataflow_service_options=enable_dynamic_thread_scaling \
     --dataflow_service_options=enable_image_streaming \
     --sdk_container_image="$CONTAINER_IMAGE_URL:latest" \
     --sdk_location=container \
