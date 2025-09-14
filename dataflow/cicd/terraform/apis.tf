b8cfd8c0b13ae840accdf345f5fb0e536c11f98a
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-09-14 01:24:13 -0700

    Do not disable APIs on destroy.

--- dataflow/cicd/terraform/apis.tf
+++ dataflow/cicd/terraform/apis.tf
@@ -2,14 +2,48 @@
 resource "google_project_service" "dataflow" {
   project = var.project_id
   service = "dataflow.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "storage" {
+  project = var.project_id
+  service = "storage-component.googleapis.com"
+
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "artifactregistry" {
   project = var.project_id
   service = "artifactregistry.googleapis.com"
+
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "containerfilesystem" {
   project = var.project_id
   service = "containerfilesystem.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "logging" {
+  project = var.project_id
+  service = "logging.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "monitoring" {
+  project = var.project_id
+  service = "monitoring.googleapis.com"
+
+  disable_on_destroy = false
 }
