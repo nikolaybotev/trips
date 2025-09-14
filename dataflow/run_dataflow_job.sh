4355b9df4f0b48afdbec01a3db43b6eca1715865
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-09-13 19:48:48 -0700

    Adjust auto-scaling to meet minimum 25GB disk size requirement.

--- dataflow/run_dataflow_job.sh
+++ dataflow/run_dataflow_job.sh
@@ -37,9 +37,9 @@ python trips_to_staypoints_dataflow.py \
     --staging_location="$STAGING_LOCATION" \
     --service_account_email="$WORKER_SERVICE_ACCOUNT" \
     --job_name="trips-to-staypoints-$(date +%Y%m%d-%H%M%S)" \
-    --min_num_workers=20 \
-    --max_num_workers=20 \
-    --disk_size_gb=20 \
+    --min_num_workers=18 \
+    --max_num_workers=18 \
+    --disk_size_gb=25 \
     --worker_disk_type=compute.googleapis.com/projects/$PROJECT_ID/zones/$REGION/diskTypes/pd-ssd \
     --experiments=use_runner_v2 \
     --dataflow_service_options=enable_prime \
